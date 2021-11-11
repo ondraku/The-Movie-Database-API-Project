4d3ea2939d10b22265a65e9243e9e682590db203
ondraku <ondra.kukla@gmail.com>
2021-11-11 21:07:51 +0100

    Created a project for the Movie Database API.

---
+++ src/main/java/com/greenfoxacademy/moviedatabaseapi/MoviedatabaseapiApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.moviedatabaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedatabaseapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviedatabaseapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenfoxacademy/moviedatabaseapi/MoviedatabaseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.moviedatabaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedatabaseapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
