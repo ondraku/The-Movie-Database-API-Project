c13144bba7489b1f9938d2b247ee386113f3fdd0
ondraku <ondra.kukla@gmail.com>
2021-11-14 12:42:10 +0100

    Initial ModelMapper configuration and dependency injection.

--- src/main/java/com/greenfoxacademy/moviedatabaseapi/service/CompanyService.java
+++ src/main/java/com/greenfoxacademy/moviedatabaseapi/service/CompanyService.java
@@ -1,7 +1,15 @@
 package com.greenfoxacademy.moviedatabaseapi.service;
 
+
+import com.greenfoxacademy.moviedatabaseapi.model.Company;
+import com.greenfoxacademy.moviedatabaseapi.model.dto.CompanyDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CompanyService {
+    CompanyDto getCompanies(Long id, String apiKey);
+
+    CompanyDto mapToDto(Company company);
+
+    Company mapToEntity(CompanyDto companyDto);
 }
--- src/main/java/com/greenfoxacademy/moviedatabaseapi/service/CompanyServiceImpl.java
+++ src/main/java/com/greenfoxacademy/moviedatabaseapi/service/CompanyServiceImpl.java
@@ -1,14 +1,46 @@
 package com.greenfoxacademy.moviedatabaseapi.service;
 
+import com.greenfoxacademy.moviedatabaseapi.model.Company;
+import com.greenfoxacademy.moviedatabaseapi.model.dto.CompanyDto;
 import com.greenfoxacademy.moviedatabaseapi.repository.CompanyRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
 
-    private final CompanyRepository companyRepository;
+    private static final String CompanyURL = "https://api.themoviedb.org/3/company/";
 
-    public CompanyServiceImpl(CompanyRepository companyRepository) {
+    private final CompanyRepository companyRepository;
+    private final RestTemplate restTemplate;
+    private final ModelMapper modelMapper;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository, RestTemplate restTemplate, ModelMapper modelMapper) {
         this.companyRepository = companyRepository;
+        this.restTemplate = restTemplate;
+        this.modelMapper = modelMapper;
     }
+
+    @Override
+    public CompanyDto getCompanies(Long id, String apiKey) {
+        ResponseEntity<CompanyDto> CompanyResponse = restTemplate
+                .getForEntity(CompanyURL + id + "?api_key=" + apiKey, CompanyDto.class);
+
+        //TODO: Mapper + Save to database
+        return CompanyResponse.getBody();
+    }
+
+    @Override
+    public CompanyDto mapToDto(Company company) {
+        return modelMapper.map(company, CompanyDto.class);
+    }
+
+    @Override
+    public Company mapToEntity(CompanyDto companyDto) {
+        return modelMapper.map(companyDto, Company.class);
+    }
+
+
 }
